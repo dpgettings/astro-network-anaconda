ff3474413a3b9dcecec9211cffa3ca103f5e8977
Dan Gettings <daniel.p.gettings@gmail.com>
2014-10-14 21:23:37 -0400

    Added conda config command to recognize env locations (and pretty printf stuff!!)

--- create_custom_anaconda_env.sh
+++ create_custom_anaconda_env.sh
@@ -3,19 +3,19 @@
 ### --------------------------------------------------------------
 ### Method 1: Create a new Python environment, cloned from default
 ### --------------------------------------------------------------
-# What is the default environment?
-conda info --envs | grep '^root' | awk '{print $NF}'
-default_env=$(conda info --envs | grep '^root' | awk '{print $NF}')
 
-# 
-# ----------------------------------
-new_env_dir=${HOME}/conda_envs   # new dir for envs
+# Set up location for custom environments
+# ---------------------------------------
+# New dir for envs
+new_env_dir=${HOME}/conda_envs   
 mkdir -p $new_env_dir
+# Tell Conda that this directory will contain Python environments
+conda config --add envs_dirs $new_env_dir
+printf '\n   %s\n' "'${new_env_dir}' added to environment locations in ~/.condarc"
 
 # Clone default env into home directory 
 # (i.e. where we have write permissions)
-
 env_name=test_env   # name for new env
 #conda create --yes --prefix ${new_env_dir}/${env_name} --clone $default_env
-conda create --yes --prefix ${new_env_dir}/${env_name} --clone root
+#conda create --yes --prefix ${new_env_dir}/${env_name} --clone root
 
